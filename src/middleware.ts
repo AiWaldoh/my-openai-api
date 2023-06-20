a2c26092368a99a00f5cc40bee0ddce361c56153
Waldoh <waldoh@gptcoder.pro>
2023-06-20 15:39:52 -0300

    added auth token for security

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,19 @@
-import express, { Express } from 'express'
+import express, { Express, Request, Response, NextFunction } from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
 import { errorHandler } from './errorHandler'
+
+const checkAuth = (req: Request, res: Response, next: NextFunction) => {
+  const bearerHeader = req.headers['authorization']
+  if (!bearerHeader) {
+    return res.status(400).send('Authorization header is required')
+  }
+  if (bearerHeader !== `Bearer ${process.env.BEARER_TOKEN}`) {
+    return res.sendStatus(403)
+  }
+  next()
+}
 
 export const applyMiddlewares = (app: Express) => {
   app.use(express.json())
@@ -13,5 +24,6 @@ export const applyMiddlewares = (app: Express) => {
     })
   )
   app.use(morgan('dev'))
+  app.use(checkAuth)
   app.use(errorHandler)
 }
